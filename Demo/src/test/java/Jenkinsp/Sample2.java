c3f1450490633fe7b7275f0ffcbd8268940ecdbd
AzmeeraSindhusravani <74254324+AzmeeraSindhusravani@users.noreply.github.com>
2021-01-03 20:56:54 +0530

    Update and rename Sample_jenkin.java to Sample2.java

---
+++ Demo/src/test/java/Jenkinsp/Sample2.java
@@ -0,0 +1,13 @@
+package Jenkinsp;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+@Test
+public void withvaliddata() {
+	System.out.println("chicken is spicy");
+	assertTrue(true);
+}
+}
